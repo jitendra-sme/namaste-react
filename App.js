8c8e079cd8cf39145b0da056cda4ebf85c96233a
Jitendra-Namaste-React <jitendra1011@outlook.com>
2023-04-21 15:53:54 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,6 +1,40 @@
-const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm an H1 tag"), React.createElement("h2", {}, "I'm an H2 tag")]), React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'm an H1 tag"), React.createElement("h2", {}, "I'm an H2 tag")])])
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+const reactElem = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React from React Element 🔥"
+);
+const jsxElem = <h2>Namaste React using JSX element</h2>;
+
+const HeadingComp = () => {
+  return (
+    <>
+      <h1 id="head" className="heading">
+        Namaste React using Component 🔥
+      </h1>
+      <span>Span tag</span>
+    </>
+  );
+};
+const Heading = () => (
+  <React.Fragment>
+    <div id="container">
+      {reactElem}
+      {jsxElem}
+      <HeadingComp />
+      <h1>Namaste React from Reach Component</h1>
+    </div>
+    <React.Fragment>
+      <div id="container2"></div>
+    </React.Fragment>
+  </React.Fragment>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent);
+root.render(<Heading />);
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2")).render(
+  <HeadingComp />
+);
